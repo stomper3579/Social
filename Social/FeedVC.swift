fe3b2b8fd95bea5a5b276c2d5b52b1b7e18ad849
Tyler Brady <tbrady@precoa.com>
2017-04-10 08:56:53 -0700

    listener for getting firebase DB changes

--- Social/FeedVC.swift
+++ Social/FeedVC.swift
@@ -19,29 +19,29 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
        
         super.viewDidLoad()
-
      
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value!)
+        })
+        
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
-    
     @IBAction func signOutTapped(_ sender: Any) {
         
-        //let keychainResult = KeychainWrapper.removeObject(<#T##KeychainWrapper#>)
-        let removeSuccessful: Bool = KeychainWrapper.standard.remove(key: KEY_UID)
+        let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         print("key removed - \(removeSuccessful)")
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier: "goToLogin", sender: nil)
