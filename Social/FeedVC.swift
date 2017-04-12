70f03ea983425d260a5d3fc3784180121503962a
Tyler Brady <tbrady@precoa.com>
2017-04-12 08:36:55 -0700

    update UI from firebase DB

--- Social/FeedVC.swift
+++ Social/FeedVC.swift
@@ -55,10 +55,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("TYLER - \(post.caption)")
         
-        
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        }
+        else {
+            return PostCell()
+        }
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
